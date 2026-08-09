2cd2b653ce2445245e71739fa67695873cc6db7d
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-08-09 15:50:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
        -> never cached, always straight to the network.
    ================================================================== */
 
-const VERSION = '8.4.1';
+const VERSION = '8.5.0';
 const CACHE   = 'samusignal-pro-v' + VERSION.replace(/\./g, '-');
 
 const SHELL = [
